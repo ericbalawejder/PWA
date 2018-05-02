ed024b1a9d3683f4039570bcd8661264720862b6
Eric Balawejder <eric.balawejder@gmail.com>
2018-05-02 15:21:58 -0400

    replaced localStorage with localforage wrapper to idb

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -45,19 +45,19 @@
   });
 
   document.getElementById('butAddCity').addEventListener('click', function() {
-      // Add the newly selected city
-      var select = document.getElementById('selectCityToAdd');
-      var selected = select.options[select.selectedIndex];
-      var key = selected.value;
-      var label = selected.textContent;
-      if (!app.selectedCities) {
-        app.selectedCities = [];
-      }
-      app.getForecast(key, label);
-      app.selectedCities.push({key: key, label: label});
-      app.saveSelectedCities();
-      app.toggleAddDialog(false);
-    });
+    // Add the newly selected city
+    var select = document.getElementById('selectCityToAdd');
+    var selected = select.options[select.selectedIndex];
+    var key = selected.value;
+    var label = selected.textContent;
+    if (!app.selectedCities) {
+      app.selectedCities = [];
+    }
+    app.getForecast(key, label);
+    app.selectedCities.push({key: key, label: label});
+    app.saveSelectedCities();
+    app.toggleAddDialog(false);
+  });
 
   document.getElementById('butAddCancel').addEventListener('click', function() {
     // Close the add new city dialog
@@ -203,7 +203,10 @@
   // Save list of cities to localStorage.
   app.saveSelectedCities = function() {
     var selectedCities = JSON.stringify(app.selectedCities);
-    localStorage.selectedCities = selectedCities;
+    
+    localforage.
+      setItem('selectedCities', selectedCities).
+      then( () => {return localforage.getItem('selectedCities');});
   };
 
   app.getIconClass = function(weatherCode) {
@@ -286,7 +289,7 @@
       },
       item: {
         condition: {
-          text: "Windy",
+          text: "Calm",
           date: "Thu, 21 Jul 2016 09:00 PM EDT",
           temp: 56,
           code: 24
@@ -325,24 +328,30 @@
    *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
    ************************************************************************/
 
-  app.selectedCities = localStorage.selectedCities;
-  if (app.selectedCities) {
-    app.selectedCities = JSON.parse(app.selectedCities);
-    app.selectedCities.forEach(function(city) {
-      app.getForecast(city.key, city.label);
-    });
-  } else {
-    /* The user is using the app for the first time, or the user has not
-     * saved any cities, so show the user some fake data. A real app in this
-     * scenario could guess the user's location via IP lookup and then inject
-     * that data into the page.
-     */
-    app.updateForecastCard(initialWeatherForecast);
-    app.selectedCities = [
-      {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
-    ];
-    app.saveSelectedCities();
-  }
+  localforage.
+  getItem('selectedCities').
+  then( 
+    function (selectedCities) {
+      app.selectedCities = selectedCities;
+      if (app.selectedCities) {
+        app.selectedCities = JSON.parse(app.selectedCities);
+        app.selectedCities.forEach(function(city) {
+          app.getForecast(city.key, city.label);
+        });
+      } else {
+        /* The user is using the app for the first time, or the user has not
+         * saved any cities, so show the user some fake data. A real app in this
+         * scenario could guess the user's location via IP lookup and then inject
+         * that data into the page.
+         */
+        app.updateForecastCard(initialWeatherForecast);
+        app.selectedCities = [
+          {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+        ];
+        app.saveSelectedCities();
+      }
+    }
+  );
 
   // TODO add service worker code here
 })();
